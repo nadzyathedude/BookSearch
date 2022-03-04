ba9374f8a7cf402f96ec3f04d1a246a7447e74fe
Chelyadinova Nadezhda <>
2022-03-04 12:07:44 +0600

    added animation on navigation between fragments

--- app/src/main/java/com/example/booksearch/ui/MainActivity.kt
+++ app/src/main/java/com/example/booksearch/ui/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.booksearch.ui
 
 import android.os.Bundle
 import android.view.Menu
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.booksearch.R
 import com.example.booksearch.databinding.ActivityMainBinding
 import com.example.booksearch.ui.base.BaseActivity
 import com.github.terrakok.cicerone.NavigatorHolder
 import com.github.terrakok.cicerone.androidx.AppNavigator
+import com.github.terrakok.cicerone.androidx.FragmentScreen
 import moxy.presenter.InjectPresenter
 import org.koin.android.ext.android.inject
 
@@ -17,7 +20,27 @@ class MainActivity : BaseActivity(), MainView {
 
     private lateinit var binding: ActivityMainBinding
     private val navigatorHolder: NavigatorHolder by inject()
-    var navigator = AppNavigator(this, R.id.main_container)
+    val navigator = object : AppNavigator(this, R.id.main_container) {
+        override fun setupFragmentTransaction(
+            screen: FragmentScreen,
+            fragmentTransaction: FragmentTransaction,
+            currentFragment: Fragment?,
+            nextFragment: Fragment
+        ) {
+            fragmentTransaction.setCustomAnimations(
+                android.R.animator.fade_in,
+                android.R.animator.fade_out,
+                android.R.animator.fade_in,
+                android.R.animator.fade_out
+            )
+            super.setupFragmentTransaction(
+                screen,
+                fragmentTransaction,
+                currentFragment,
+                nextFragment
+            )
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
